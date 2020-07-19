c232a4f454f7d4b46314f25d5a47aaadf0b2cf6f
deepalidsp <dipali.kolhe51@gmail.com>
2020-07-19 12:35:24 +0530

    about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,29 +1,15 @@
 <div class="about" id="about">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 text-center">
-          <h3>About Us</h3>
-        </div>
-        <div class="col-md-6">
-          <div class="about-text">
-            <p>Donec velit ex, faucibus eu mauris in, sodales placerat augue. Phasellus feugiat ex id enim laoreet mattis. Quisque velit quam, pharetra non lorem vel, scelerisque ornare dolor. Etiam id ex justo. Nullam nec ipsum non augue convallis gravida. Praesent mattis placerat sodales. Pellentesque nec egestas neque. </p>
-            <p>Donec velit ex, faucibus eu mauris in, sodales placerat augue. Phasellus feugiat ex id enim laoreet mattis. Quisque velit quam, pharetra non lorem vel, scelerisque ornare dolor. Etiam id ex justo. Nullam nec ipsum non augue convallis gravida. Praesent mattis placerat sodales. Pellentesque nec egestas neque. </p>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="col-md-6 col-sm-6 col-xs-6 block">
-            <div class="counter-item text-center">
-              <h5>Our Clients</h5>
-              <div class="timer" data-from="0" data-to="55" data-speed="5000" data-refresh-interval="50"></div>
-            </div>
-          </div>
-          <div class="col-md-6 col-sm-6 col-xs-6">
-            <div class="counter-item text-center">
-              <h5>Projects completed</h5>
-              <div class="timer" data-from="0" data-to="88" data-speed="5000" data-refresh-interval="50"></div>
-            </div>
-          </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h3>About Us</h3>
+      </div>
+      <div class="col-md-12">
+        <div class="about-text">
+          <p>Donec velit ex, faucibus eu mauris in, sodales placerat augue. Phasellus feugiat ex id enim laoreet mattis. Quisque velit quam, pharetra non lorem vel, scelerisque ornare dolor. Etiam id ex justo. Nullam nec ipsum non augue convallis gravida. Praesent mattis placerat sodales. Pellentesque nec egestas neque. </p>
+          <p>Donec velit ex, faucibus eu mauris in, sodales placerat augue. Phasellus feugiat ex id enim laoreet mattis. Quisque velit quam, pharetra non lorem vel, scelerisque ornare dolor. Etiam id ex justo. Nullam nec ipsum non augue convallis gravida. Praesent mattis placerat sodales. Pellentesque nec egestas neque. </p>
         </div>
       </div>
     </div>
   </div>
+</div>
