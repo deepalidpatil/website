c24b892be78de146302bf2f07e2dee723a0a582c
deepalidsp <dipali.kolhe51@gmail.com>
2020-07-19 14:33:43 +0530

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,17 +20,9 @@
           
                 navigation : false, // Show next and prev buttons
                 slideSpeed : 500,
-            autoPlay : 3000,
+                autoPlay : 3000,
                 paginationSpeed : 400,
                 singleItem:true
-          
-                // "singleItem:true" is a shortcut for:
-                // items : 1, 
-                // itemsDesktop : false,
-                // itemsDesktopSmall : false,
-                // itemsTablet: false,
-                // itemsMobile : false
-          
             });
           
           });
